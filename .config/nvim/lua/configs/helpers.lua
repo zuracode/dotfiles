96eadefe05462ade1279e9113056b7f10ec95079
zuracode <denizen_launch0b@icloud.com>
2025-12-19 03:11:13 +0400

    add graphql helpers

--- .config/nvim/lua/configs/helpers.lua
+++ .config/nvim/lua/configs/helpers.lua
@@ -14,6 +14,10 @@ local function insertImmutable(tbl, value)
   return newTable
 end
 
+local function escape_wildcards(path)
+  return path:gsub('([%[%]%?%*])', '\\%1')
+end
+
 M.nvim_eleven = vim.fn.has 'nvim-0.11' == 1
 
 M.root_markers_git = '.git'
@@ -37,6 +41,58 @@ function M.root_dir_monorepo(bufnr, on_dir)
   local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
 
   on_dir(project_root)
+end
+
+-- For zipfile: or tarfile: virtual paths, returns the path to the archive.
+-- Other paths are returned unaltered.
+local function strip_archive_subpath(path)
+  path = vim.fn.substitute(path, 'zipfile://\\(.\\{-}\\)::[^\\\\].*$', '\\1', '')
+  path = vim.fn.substitute(path, 'tarfile:\\(.\\{-}\\)::.*$', '\\1', '')
+  return path
+end
+
+local function search_ancestors(startpath, func)
+  if M.nvim_eleven then
+    local validate = vim.validate
+    validate('func', func, 'function')
+  end
+  if func(startpath) then
+    return startpath
+  end
+  local guard = 100
+  for path in vim.fs.parents(startpath) do
+    guard = guard - 1
+    if guard == 0 then
+      return
+    end
+    if func(path) then
+      return path
+    end
+  end
+end
+
+local function tbl_flatten(t)
+  return M.nvim_eleven and vim.iter(t):flatten(math.huge):totable() or vim.tbl_flatten(t)
+end
+
+function M.root_pattern(...)
+  local patterns = tbl_flatten { ... }
+  return function(startpath)
+    startpath = strip_archive_subpath(startpath)
+    for _, pattern in ipairs(patterns) do
+      local match = search_ancestors(startpath, function(path)
+        for _, p in ipairs(vim.fn.glob(table.concat({ escape_wildcards(path), pattern }, '/'), true, true)) do
+          if vim.uv.fs_stat(p) then
+            return path
+          end
+        end
+      end)
+      if match ~= nil then
+        local real = vim.uv.fs_realpath(match)
+        return real or match
+      end
+    end
+  end
 end
 
 function M.root_markers_with_field(root_files, new_names, field, fname)
