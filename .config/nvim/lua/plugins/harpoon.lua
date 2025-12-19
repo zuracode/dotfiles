036d2d1520da92444cffcd3a94cfe0d565deda91
zuracode <denizen_launch0b@icloud.com>
2025-12-20 00:07:32 +0400

    remove harpoon keymaps, was coliding with tmux navigation

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -7,16 +7,10 @@ return {
     harpoon:setup()
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end,
       { desc = "Harpoon: Add mark of buffer in harpoon list" })
-    vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon:" })
-
-    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, { desc = "Harpoon:" })
-    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end, { desc = "Harpoon:" })
-    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end, { desc = "Harpoon:" })
-    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end, { desc = "Harpoon:" })
 
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Harpoon:" })
-    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Harpoon:" })
+    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "Harpoon: toggle to previous buffer" })
+    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "Harpoon: toggle to next buffer" })
   end,
   keys = function()
     local keys = {
