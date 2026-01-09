d072db9d08db44b10c0bfe36008fc61365c2db03
zuracode <denizen_launch0b@icloud.com>
2026-01-09 22:20:00 +0400

    make lsp and linter changes

--- .config/nvim/lsp/solargraph.lua
+++ .config/nvim/lsp/solargraph.lua
@@ -1,13 +1,12 @@
 local helpers = require "configs.helpers"
 
 return {
-  cmd = { 'solargraph', 'stdio' },
-  settings = {
-    solargraph = {
-      diagnostics = true,
+    cmd = { 'solargraph', 'stdio' },
+    settings = {
+        solargraph = {
+            diagnostics = true,
+        },
     },
-  },
-  init_options = { formatting = true },
-  filetypes = { 'ruby' },
-  root_markers = helpers.root_markers_ruby_with_git
+    filetypes = { 'ruby' },
+    root_markers = helpers.root_markers_ruby_with_git
 }
--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,29 +1,29 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = { "davidmh/cspell.nvim" },
-  config = function()
-    local cspell = require('cspell')
+    "nvimtools/none-ls.nvim",
+    dependencies = { "davidmh/cspell.nvim" },
+    config = function()
+        local cspell = require('cspell')
 
-    local config = {
-      config_file_preferred_name = 'cspell.json',
-      cspell_config_dirs = { "~/.config/" }
-    }
-    require("null-ls").setup {
-      sources = {
-        cspell.diagnostics.with({ config = config, diagnostics_postprocess = function(diagnostic)
-          diagnostic.severity = vim.diagnostic.severity["INFO"]
-        end, }),
-        cspell.code_actions.with({ config = config }),
-      }
-    }
-    table.insert(
-      {},
-      cspell.diagnostics.with({
-        diagnostics_postprocess = function(diagnostic)
-          diagnostic.severity = vim.diagnostic.severity["INFO"]
-        end,
-      })
-    )
-    table.insert({}, cspell.code_actions)
-  end
+        local config = {
+            config_file_preferred_name = 'cspell.json',
+            cspell_config_dirs = { "~/.config/" }
+        }
+        require("null-ls").setup {
+            sources = {
+                cspell.diagnostics.with({ config = config, diagnostics_postprocess = function(diagnostic)
+                    diagnostic.severity = vim.diagnostic.severity["INFO"]
+                end, }),
+                cspell.code_actions.with({ config = config }),
+            }
+        }
+        table.insert(
+            {},
+            cspell.diagnostics.with({
+                diagnostics_postprocess = function(diagnostic)
+                    diagnostic.severity = vim.diagnostic.severity["INFO"]
+                end,
+            })
+        )
+        table.insert({}, cspell.code_actions)
+    end
 }
