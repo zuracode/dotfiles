3c21fcf5d8658a3e2c498fc9e9b9bc9f368188a4
zuracode <denizen_launch0b@icloud.com>
2026-01-11 00:40:01 +0400

    add fix of indentation and add fold save

--- .config/nvim/lua/configs/autocmd.lua
+++ .config/nvim/lua/configs/autocmd.lua
@@ -6,6 +6,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
+  pattern = { '*.*' },
+  desc = 'save view (folds), when closing file',
+  command = 'mkview',
+})
+
+vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+  pattern = { '*.*' },
+  desc = 'load view (folds), when opening file',
+  command = 'silent! loadview',
+})
+
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile', 'InsertLeave', 'TextChanged' }, {
   pattern = '*.md',
   callback = function()
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,6 +2,8 @@
 
 return {
   'folke/snacks.nvim',
+  lazy = false,
+  priority = 1000,
   ---@type snacks.Config
   opts = {
     image = { enabled = true },
@@ -13,21 +15,42 @@ return {
           vim.api.nvim_set_current_win(self.main)
           vim.system({ 'tmux', 'select-pane', '-L' })
         end,
+        tmux_down = function(self)
+          vim.api.nvim_set_current_win(self.main)
+          vim.system({ 'tmux', 'select-pane', '-D' })
+        end,
+        tmux_up = function(self)
+          vim.api.nvim_set_current_win(self.main)
+          vim.system({ 'tmux', 'select-pane', '-U' })
+        end,
+        tmux_right = function(self)
+          vim.api.nvim_set_current_win(self.main)
+          vim.system({ 'tmux', 'select-pane', '-R' })
+        end,
       },
       win = {
         input = {
           keys = {
             ['<c-h>'] = { 'tmux_left', mode = { 'i', 'n' } },
+            ['<c-j>'] = { 'tmux_down', mode = { 'i', 'n' } },
+            ['<c-k>'] = { 'tmux_up', mode = { 'i', 'n' } },
+            -- ['<c-l>'] = { 'tmux_right', mode = { 'i', 'n' } },
           },
         },
         list = {
           keys = {
             ['<c-h>'] = { 'tmux_left', mode = { 'n', 'x' } },
+            ['<c-j>'] = { 'tmux_down', mode = { 'n', 'x' } },
+            ['<c-k>'] = { 'tmux_up', mode = { 'n', 'x' } },
+            -- ['<c-l>'] = { 'tmux_right', mode = { 'n', 'x' } },
           },
         },
         preview = {
           keys = {
             ['<c-h>'] = 'tmux_left',
+            ['<c-j>'] = 'tmux_down',
+            ['<c-k>'] = 'tmux_up',
+            -- ['<c-l>'] = 'tmux_right',
           },
         },
       },
@@ -39,7 +62,7 @@ return {
       function()
         Snacks.explorer()
       end,
-      desc = 'Explorer',
+      desc = 'Explorer open',
     },
   },
 }
