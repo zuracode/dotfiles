8c8ef35d8663c91a8b9bcfe80fd5d955d3cfaa0d
zuracode <denizen_launch0b@icloud.com>
2026-01-11 00:12:39 +0400

    remove keymap additions from snack picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -13,42 +13,21 @@ return {
           vim.api.nvim_set_current_win(self.main)
           vim.system({ 'tmux', 'select-pane', '-L' })
         end,
-        tmux_down = function(self)
-          vim.api.nvim_set_current_win(self.main)
-          vim.system({ 'tmux', 'select-pane', '-D' })
-        end,
-        tmux_up = function(self)
-          vim.api.nvim_set_current_win(self.main)
-          vim.system({ 'tmux', 'select-pane', '-U' })
-        end,
-        tmux_right = function(self)
-          vim.api.nvim_set_current_win(self.main)
-          vim.system({ 'tmux', 'select-pane', '-R' })
-        end,
       },
       win = {
         input = {
           keys = {
             ['<c-h>'] = { 'tmux_left', mode = { 'i', 'n' } },
-            ['<c-j>'] = { 'tmux_down', mode = { 'i', 'n' } },
-            ['<c-k>'] = { 'tmux_up', mode = { 'i', 'n' } },
-            ['<c-l>'] = { 'tmux_right', mode = { 'i', 'n' } },
           },
         },
         list = {
           keys = {
             ['<c-h>'] = { 'tmux_left', mode = { 'n', 'x' } },
-            ['<c-j>'] = { 'tmux_down', mode = { 'n', 'x' } },
-            ['<c-k>'] = { 'tmux_up', mode = { 'n', 'x' } },
-            ['<c-l>'] = { 'tmux_right', mode = { 'n', 'x' } },
           },
         },
         preview = {
           keys = {
             ['<c-h>'] = 'tmux_left',
-            ['<c-j>'] = 'tmux_down',
-            ['<c-k>'] = 'tmux_up',
-            ['<c-l>'] = 'tmux_right',
           },
         },
       },
